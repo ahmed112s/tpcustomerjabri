47d3c6c8e1a5f7feae61b56a0be1fbc41a991650
EL JABRI <90796656+ahmed112s@users.noreply.github.com>
2022-02-01 14:50:26 +0100

    Ajout EJB CustomerManager

---
+++ src/main/java/com/fr/insset/tpcustomerjabri/session/CustomerManager.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package com.fr.insset.tpcustomerjabri.session;
+
+import com.fr.insset.tpcustomerjabri.entities.Customer;
+import javax.ejb.Stateless;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
+/**
+ *
+ * @author Ahmed
+ */
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+    
+    public List<Customer> getAllCustomers() {
+      Query query = em.createNamedQuery ("Customer.findAll");
+      return query.getResultList();  
+    }  
+        
+    public Customer update(Customer customer) {
+      return em.merge(customer);  
+    }  
+
+    public void persist(Customer customer) {
+            em.persist(customer);
+    }
+}
